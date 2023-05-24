a0bf41c9037c9a69aab5c137551633a53041f769
Naoki0722 <mattu.nao722@gmail.com>
2023-05-25 08:51:06 +0900

    一部修正

--- react-admin/src/admin/users.tsx
+++ react-admin/src/admin/users.tsx
@@ -1,15 +1,16 @@
 import { VFC } from 'react'
-import { Datagrid, EmailField, List, TextField, UrlField } from 'react-admin'
+import { Datagrid, EditButton, EmailField, List, TextField, UrlField } from 'react-admin'
 
 export const Users: VFC = () => (
   <List>
-    <Datagrid rowClick="edit">
+    <Datagrid rowClick="show">
       <TextField source="id" />
       <TextField source="name" />
       <EmailField source="email" />
       <TextField source="phone" />
       <UrlField source="website" />
       <TextField source="company.name" />
+      <EditButton />
     </Datagrid>
   </List>
 )
