13e65c95390d3d64d7b78c76dbeecc706849d8d4
Naoki0722 <mattu.nao722@gmail.com>
2023-05-25 09:35:43 +0900

    ダッシュボード画面追加

---
+++ react-admin/src/admin/dashboard.tsx
@@ -0,0 +1,5 @@
+import { Title } from 'react-admin'
+
+const Dashboard = () => <Title title="Welcome to the administration" />
+
+export default Dashboard
--- react-admin/src/index.tsx
+++ react-admin/src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
+const router = createBrowserRouter([
+  {
+    path: '/*',
+    element: <App />,
+  },
+])
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('root')
 )
