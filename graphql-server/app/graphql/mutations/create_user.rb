000a25a9de5d7380188e67853fd0aef300c2e0cf
Naoki0722 <mattu.nao722@gmail.com>
2023-06-15 10:43:05 +0900

    サーバーバリデーション

--- graphql-server/app/graphql/mutations/create_user.rb
+++ graphql-server/app/graphql/mutations/create_user.rb
@@ -1,7 +1,8 @@
 module Mutations
   class CreateUser < BaseMutation
     # TODO: define return fields
-    field :user, Types::UserType, null: false
+    field :user, Types::UserType, null: true
+    field :errors, [Types::ErrorType] , null: true
 
     # TODO: define arguments
     argument :email, String, required: true
@@ -9,9 +10,30 @@ module Mutations
     argument :last_name, String, required: true
     argument :age, Integer, required: true
 
+
     def resolve(**params)
-      user = User.create!(params)
-      { user: user }
+      user = User.new(params)
+      if user.valid?
+        user.save!
+        return {
+          user: user,
+          errors: [],
+        }
+      else
+        p user.errors.details
+        errors = user.errors.details.map do |attribute, details|
+          details.map do |error_detail|
+            {
+              field: attribute.to_s,
+              message: user.errors.full_message(attribute, error_detail[:error])
+            }
+          end
+        end.flatten
+        return {
+          user: nil,
+          errors: errors,
+        }
+      end
     end
   end
 end
--- graphql-server/app/models/user.rb
+++ graphql-server/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :movies, dependent: :destroy
+  validates :email, presence: true, length: { minimum: 10 }
+  validates :first_name, presence: true
 end
