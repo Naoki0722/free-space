57e5e8937fd5d62a268157b5f9dee5b9350f6881
Naoki0722 <mattu.nao722@gmail.com>
2023-08-11 23:20:38 +0900

    add

---
+++ commander-js/index-3.ts
@@ -0,0 +1,15 @@
+import { Command } from "commander";
+const program = new Command();
+
+// バージョン情報
+program.version("0.0.1", "-v, --version");
+
+// hogeコマンド
+program
+  .command("hoge")
+  .description("my first example")
+  .action(() => console.log("run hoge command"));
+
+program.parse();
+
+console.log(program.args);
